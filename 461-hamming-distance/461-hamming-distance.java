661fabbb29926244c68785fd157b802b29a001a4
Priyanka Giri <80326865+itspgiri@users.noreply.github.com>
2022-05-19 11:59:18 +0400

    Time: 0 ms (100.00%), Space: 39.8 MB (74.79%) - LeetHub

---
+++ 461-hamming-distance/461-hamming-distance.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+    int con = 0;
+    while(x != 0 || y != 0) //We keep OR so that the looks doesn't fail even if one of them become 0
+    {
+        int t = x&1;  //Extracts the last Bit of X
+        int u = y&1; //Extracts the last Bit of Y
+        if((t ^ u) == 1)   //XOR returns 1 whenever they are different bits, 0 for same.
+            con++;                 // Different bit? Increment our counter.
+        x = x>>1; //Now shifting our value of x to remove the last bit for our next iteration.
+        y = y>>1; //Now shifting our value of Y to remove the last bit for our next iteration.
+    }
+    return con;
+    }
+}
