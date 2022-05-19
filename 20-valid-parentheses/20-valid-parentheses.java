2432ced5e8803e6188ae26f3f35ea541c9025b99
Priyanka Giri <80326865+itspgiri@users.noreply.github.com>
2022-05-19 20:59:56 +0400

    Time: 66 ms (5.29%), Space: 54 MB (6.05%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public boolean isValid(String s) {
+        int length;
+    
+        do {
+            length = s.length();
+            s = s.replace("()", "").replace("{}", "").replace("[]", "");
+        } while(length != s.length());
+    
+        return s.length() == 0;
+    }
+}
