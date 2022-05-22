22fa1ab4b252201d61e3ea091c08be44d32b5979
Priyanka Giri <80326865+itspgiri@users.noreply.github.com>
2022-05-22 12:10:33 +0400

    Time: 66 ms (25.00%), Space: 106.5 MB (37.50%) - LeetHub

--- 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.java
+++ 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.java
@@ -5,11 +5,15 @@ class Solution {
         Arrays.sort(stockPrices, (a, b) -> Integer.compare(a[0], b[0]));
         int count = 1;
         for (int i = 2; i < stockPrices.length; i++) {
-
-            if ((stockPrices[i][1] - stockPrices[i - 1][1]) * (stockPrices[i - 1][0] - stockPrices[i - 2][0]) == 
-                (stockPrices[i - 1][1] - stockPrices[i - 2][1]) * (stockPrices[i][0] - stockPrices[i - 1][0])) 
+            //AVOID DIVINDING AT ALL CASES BASICALLY IS A KEY LEARNING OF THIS QUESTION
+            int x1 = stockPrices[i-2][0];
+            int x2 = stockPrices[i-1][0];
+            int x3 = stockPrices[i][0];
+            int y1 = stockPrices[i-2][1];
+            int y2 = stockPrices[i-1][1];
+            int y3 = stockPrices[i][1];
+            if ((y3-y2) * (x2-x1) == (y2-y1)*(x3-x2))
                 continue;
-            
             count++;;
         }
         return count;
