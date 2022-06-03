d0b5d0c3d1b7347d47f124d12ef92b2acb615a2f
Priyanka Giri <80326865+itspgiri@users.noreply.github.com>
2022-06-03 12:07:03 +0530

    Time: 2 ms (39.75%), Space: 53.8 MB (62.99%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,22 +1,42 @@
 class Solution {
 
     public void setZeroes(int[][] matrix) {
-        int rows = matrix.length, cols = matrix[0].length;
-        int dummy1[] = new int[rows];
-        int dummy2[] = new int[cols];
-        Arrays.fill(dummy1, -1);
-        Arrays.fill(dummy2, -1);
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int answer[][] = new int[n][m];
+        // Set all elements of answer array as 1
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                answer[i][j] = 1;
+            }
+        }
+        // Traverse row wise
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 if (matrix[i][j] == 0) {
-                    dummy1[i] = 0;
-                    dummy2[j] = 0;
+                    // Set this row as zero in answer array
+                    for (int k = 0; k < m; k++) {
+                        answer[i][k] = 0;
+                    }
+                    break;
                 }
             }
         }
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (dummy1[i] == 0 || dummy2[j] == 0) {
+        // Traverse column wise
+        for (int j = 0; j < m; j++) {
+            for (int i = 0; i < n; i++) {
+                if (matrix[i][j] == 0) {
+                    // Set this column as 0 in answer array
+                    for (int k = 0; k < n; k++) {
+                        answer[k][j] = 0;
+                    }
+                }
+            }
+        }
+        // Update the elements in matrix array
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (answer[i][j] == 0) {
                     matrix[i][j] = 0;
                 }
             }
