947d25408464fe65fc48fc3c4048445b9429fb93
Priyanka Giri <80326865+itspgiri@users.noreply.github.com>
2022-06-03 20:25:13 +0530

    Time: 1 ms (56.88%), Space: 41.6 MB (55.97%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.java
+++ 118-pascals-triangle/118-pascals-triangle.java
@@ -1,26 +1,20 @@
 class Solution {
-    public List<List<Integer>> generate(int n) {
-        List<List<Integer>> ans = new ArrayList<>();
-		if(n==0) 
-        {
-			return new ArrayList<>();
-		}
-		
-		for(int i=1;i<=n;i++) {
-			List<Integer> row = new ArrayList<>();
-			for(int j=0;j<i;j++) {
-				if(j==0 || j==i-1) 
-                {
-					row.add(1);  
-                    // as 1 is present at the beginning and  ending of each row 
-				}
-				else {
-					int sum = ans.get(i-2).get(j) + ans.get(i-2).get(j-1); // this is the sum we got from the above row 											by adding jth index and (j-1)th index
-					row.add(sum);
-				}
-			}
-			ans.add(row);
-		}
-		return ans;
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        
+        for(int i = 0; i < numRows; i++) {
+            Integer[] row = new Integer[i + 1];
+            row[0] = 1;
+            row[i] = 1;
+            if(i > 0) {
+                List<Integer> previousRow = result.get(i - 1);
+                for(int j = 1; j < row.length - 1; j++) {
+                    row[j] = previousRow.get(j - 1) + previousRow.get(j);
+                }
+            }
+            result.add(Arrays.asList(row));
+        }
+        
+        return result;
     }
 }
