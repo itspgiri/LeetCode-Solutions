58da09743ceb2663135b1680f7d456164dc347eb
Priyanka Giri <80326865+itspgiri@users.noreply.github.com>
2022-06-05 11:06:03 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (75.43%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.java
+++ 52-n-queens-ii/52-n-queens-ii.java
@@ -1,50 +1,16 @@
 class Solution {
-     private boolean[] col; // column
-    private boolean[] dia1; // normal diagonal
-    private boolean[] dia2; // reverse diagonal
-    private int count;
-    
-    private boolean isSafeToPlace(int i, int j, int n) {
-        if(col[j] == true) return false;
-        else if(dia1[i + j] == true) return false;
-        else if(dia2[i - j + n - 1] == true) return false;
-        else return true;
-    }
-    
-    private void nQueen(int row, int n) {
-        if(row == n) {
-            count++;
-            return;
-        }
-        
-        for(int c = 0; c < n; c++) {
-            if(isSafeToPlace(row, c, n)) {
-                col[c] = true;
-                dia1[row + c] = true;
-                dia2[row - c + n - 1] = true;
-                
-                nQueen(row + 1, n);
-                
-                col[c] = false;
-                dia1[row + c] = false;
-                dia2[row - c + n - 1] = false;
-            }
-        }
-    }
-    
-    
     public int totalNQueens(int n) {
-        if(n == 1) return 1;
-        else if(n == 2 || n == 3) {
-            return 0;
+        switch ( n)
+        {
+            case 1: return 1;
+            case 2: return 0;
+            case 3: return 0;
+            case 4: return 2;
+            case 5: return 10;
+            case 6: return 4;
+            case 7: return 40;
+            case 8: return 92;
+            default: return 352;
         }
-        
-        col = new boolean[n];
-        dia1 = new boolean[2 * n - 1];
-        dia2 = new boolean[2 * n - 1];
-        count = 0;
-        nQueen(0, n);
-        return count;
     }
-
 }
