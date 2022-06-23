d42dca99c92174e408e519795b8a99707c05cdca
Priyanka Giri <80326865+itspgiri@users.noreply.github.com>
2022-06-23 12:56:13 +0530

    Time: 56 ms (58.89%), Space: 49.7 MB (96.67%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.java
+++ 630-course-schedule-iii/630-course-schedule-iii.java
@@ -1,24 +1,13 @@
 public class Solution {
     public int scheduleCourse(int[][] courses) {
-        System.out.println(courses.length);
-        Arrays.sort(courses, (a, b) -> a[1] - b[1]);
-        int time = 0, count = 0;
-        for (int i = 0; i < courses.length; i++) {
-            if (time + courses[i][0] <= courses[i][1]) {
-                time += courses[i][0];
-                courses[count++] = courses[i];
-            } else {
-                int max_i = i;
-                for (int j = 0; j < count; j++) {
-                    if (courses[j][0] > courses[max_i][0])
-                        max_i = j;
-                }
-                if (courses[max_i][0] > courses[i][0]) {
-                    time += courses[i][0] - courses[max_i][0];
-                    courses[max_i] = courses[i];
-                }
-            }
-        }
-        return count;
-    }
+	var maxHeap = new PriorityQueue<Integer>(Comparator.reverseOrder());
+	Arrays.sort(courses, Comparator.comparingInt(course -> course[1]));
+
+	for (int i = 0, total = 0; i < courses.length; i++) {
+		maxHeap.add(courses[i][0]);
+		if ((total += courses[i][0]) > courses[i][1])
+			total -= maxHeap.poll();
+	}
+	return maxHeap.size();
+}
 }
