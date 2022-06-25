d821baee7039de2cff5e110c55db8f5a7045391e
Priyanka Giri <80326865+itspgiri@users.noreply.github.com>
2022-06-25 08:59:52 +0530

    Time: 1 ms (97.53%), Space: 43.2 MB (95.29%) - LeetHub

---
+++ 665-non-decreasing-array/665-non-decreasing-array.java
@@ -0,0 +1,12 @@
+class Solution {
+   public boolean checkPossibility(int[] nums) {
+	for (int i=1, modified=0, prev = nums[0]; i<nums.length; i++) {
+		if (nums[i] < prev) {
+			if (modified++ == 1) return false;
+			if (i>=2 && nums[i-2] > nums[i]) continue;
+		}
+		prev = nums[i];
+	}
+	return true;
+}
+}
