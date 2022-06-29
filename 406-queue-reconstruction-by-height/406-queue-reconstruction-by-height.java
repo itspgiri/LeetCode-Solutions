26aa387a7cc447bda5f349bbe54d6c12cca9b767
Priyanka Giri <80326865+itspgiri@users.noreply.github.com>
2022-06-29 08:39:59 +0530

    Time: 11 ms (56.27%), Space: 54.6 MB (52.20%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.java
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.java
@@ -1,27 +1,17 @@
 class Solution {
-      public int[][] reconstructQueue(int[][] people) {
-        if (people == null || people.length == 0 || people[0].length == 0)
-            return new int[0][0];
-            
-        Arrays.sort(people, new Comparator<int[]>() {
-            public int compare(int[] a, int[] b) {
-                if (b[0] == a[0]) return a[1] - b[1];
-                return b[0] - a[0];
-            }
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, (c1, c2) -> {
+           if (c1[0] == c2[0]) {
+               return c1[1] - c2[1];
+           } else {
+               return c2[0] - c1[0];
+           }
         });
         
-        int n = people.length;
-        ArrayList<int[]> tmp = new ArrayList<>();
-        for (int i = 0; i < n; i++)
-            tmp.add(people[i][1], new int[]{people[i][0], people[i][1]});
-
-        int[][] res = new int[people.length][2];
-        int i = 0;
-        for (int[] k : tmp) {
-            res[i][0] = k[0];
-            res[i++][1] = k[1];
+        List<int[]> res = new ArrayList<>();
+        for (int[] person : people) {
+            res.add(person[1], person);
         }
-        
-        return res;
+        return res.toArray(new int[1][1]);
     }
 }
