0323afde1d98ed00a69ec8decf3b7fce96b6cbaf
Priyanka Giri <80326865+itspgiri@users.noreply.github.com>
2022-07-01 20:27:52 +0400

    Time: 14 ms (23.56%), Space: 47.1 MB (58.83%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -1,15 +1,16 @@
 public class Solution {
+
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root==null) return null;
-        if(root==p) return root;
-        if(root==q) return root;
+        if (root == null || root == p || root == q) // found p or q or touch the ground
+        return root;
+
+        // search p and q from left and right
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
-        if(left!=null&&right!=null){
-            return root;
-        }else
-        {
-            return left==null?right:left;
-        }
+
+        if (left != null && right != null) // from root's left & right we found both p and q, so root is the LCA
+        return root; else // left is not null means from left's left & right we found both q and q
+        // so left is the LCA, otherwise, right is the answer
+        return left != null ? left : right;
     }
 }
