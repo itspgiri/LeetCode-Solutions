672c65c355976d5bcf9e69835067ffc4b26f869c
Priyanka Giri <80326865+itspgiri@users.noreply.github.com>
2022-07-20 12:57:43 +0400

    Time: 110 ms (81.46%), Space: 118.7 MB (11.55%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.java
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.java
@@ -1,46 +1,18 @@
+import java.text.StringCharacterIterator;
+
 class Solution {
-    class TrieNode {
-        char c;
-        int end;
-        TrieNode[] children;
-        public TrieNode(char c) {
-            this.c = c;
-            this.end = 0;
-            this.children = new TrieNode[26];
-        }
-    }
     public int numMatchingSubseq(String S, String[] words) {
-        if (words == null || words.length == 0) return 0;
-        TrieNode root = createTrie(words);
-        return dfs(S, root, 0);
+    List<StringCharacterIterator>[] waiting = new List[128];
+    for (int c = 0; c <= 'z'; c++)
+        waiting[c] = new ArrayList();
+    for (String w : words)
+        waiting[w.charAt(0)].add(new StringCharacterIterator(w));
+    for (char c : S.toCharArray()) {
+        List<StringCharacterIterator> advance = waiting[c];
+        waiting[c] = new ArrayList();
+        for (StringCharacterIterator it : advance)
+            waiting[it.next() % it.DONE].add(it);
     }
-    
-    private TrieNode createTrie(String[] words) {
-        TrieNode root = new TrieNode('*');
-        for (String word : words) {
-            addWord(root, word);
-        }
-        return root;
-    }
-    
-    private void addWord(TrieNode root, String word) {
-        for (char c : word.toCharArray()) {
-            if (root.children[c - 'a'] == null) {
-                root.children[c - 'a'] = new TrieNode(c);
-            }
-            root = root.children[c - 'a'];
-        }
-        root.end++;
-    }
-    
-    private int dfs(String S, TrieNode root, int pos) {
-        if (root == null) return 0;
-        int index = S.indexOf(root.c, pos);
-        if (root.c != '*' && index == -1) return 0;
-        int res = root.end;
-        for (int i = 0; i < 26; i++) {
-            res += dfs(S, root.children[i], root.c == '*' ? 0 : index + 1);
-        }
-        return res;
-    }
+    return waiting[0].size();
+}
 }
