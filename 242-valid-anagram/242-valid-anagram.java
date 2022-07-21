4ba0d2028b35ee62167bad956fedb069664a9b4b
Priyanka Giri <80326865+itspgiri@users.noreply.github.com>
2022-07-21 08:20:59 +0400

    Time: 2 ms (99.09%), Space: 44 MB (51.50%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,17 +1,17 @@
-public class Solution {
+class Solution {
+
     public boolean isAnagram(String s, String t) {
-        if (s==null && t==null) return true;
-        else if (s==null || t==null) return false;
-        else if (s.length() != t.length()) return false;
-
-        final Map<Integer, Integer> dict = new HashMap<>();
-        s.codePoints().forEach(code -> dict.put(code, dict.getOrDefault(code, 0) + 1));
-        t.codePoints().forEach(code -> dict.put(code, dict.getOrDefault(code, 0) - 1));
-        
-        for(int count : dict.values()) {
-            if (count<0) return false;
+        int as[] = new int[26];
+        for (char a : s.toCharArray()) {
+            as[a - 'a'] += 1;
         }
-
+        for (char a : t.toCharArray()) {
+            as[a - 'a'] -= 1;
+        }
+        for (int x : as)
+            if (x!=0)
+                return false;
+        
         return true;
     }
 }
