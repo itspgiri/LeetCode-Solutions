9f0942aa2163a7932fb97c4637364296b5190e85
Priyanka Giri <80326865+itspgiri@users.noreply.github.com>
2022-07-21 07:08:45 +0400

    Time: 12 ms (61.82%), Space: 68.5 MB (67.43%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,27 +1,5 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        // O(N^2)
-        // for (int i=0;i<nums.length;i++)
-        // {
-        //     for (int j=0; j<nums.length; j++)
-        //     {
-        //         if (i!=j)
-        //             if (nums[i] == nums[j])
-        //                 return true;
-        //     }
-        // }
-        // return false;
-        
-        //O(NLogN)
-        // Arrays.sort(nums);
-        // for (int i=1; i<nums.length; i++)
-        // {
-        //     if (nums[i-1]==nums[i])
-        //         return true;
-        // }
-        // return false;
-        
-        //O(N) TC + O(N) SC
         HashSet<Integer> s = new HashSet<Integer>();
         for (int x : nums)
         {
