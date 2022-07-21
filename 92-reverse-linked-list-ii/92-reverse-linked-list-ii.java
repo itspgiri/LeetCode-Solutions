ddb8ac67932afe4972253ab54d440a5f0b6e28eb
Priyanka Giri <80326865+itspgiri@users.noreply.github.com>
2022-07-21 08:15:20 +0400

    Time: 0 ms (100.00%), Space: 42.3 MB (13.41%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.java
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.java
@@ -1,52 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-
-    // Object level variables since we need the changes
-    // to persist across recursive calls and Java is pass by value.
-    private boolean stop;
-    private ListNode left;
-
-    public void recurseAndReverse(ListNode right, int m, int n) {
-
-        // base case. Don't proceed any further
-        if (n == 1) {
-            return;
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+       ListNode dummy = new ListNode(0); // created dummy node
+        dummy.next = head;
+        ListNode prev = dummy; // intialising prev pointer on dummy node
+        
+        for(int i = 0; i < left - 1; i++)
+            prev = prev.next; // adjusting the prev pointer on it's actual index
+        
+        ListNode curr = prev.next; // curr pointer will be just after prev
+        // reversing
+        for(int i = 0; i < right - left; i++){
+            ListNode forw = curr.next; // forw pointer will be after curr
+            curr.next = forw.next;
+            forw.next = prev.next;
+            prev.next = forw;
         }
-
-        // Keep moving the right pointer one step forward until (n == 1)
-        right = right.next;
-
-        // Keep moving left pointer to the right until we reach the proper node
-        // from where the reversal is to start.
-        if (m > 1) {
-            this.left = this.left.next;
-        }
-
-        // Recurse with m and n reduced.
-        this.recurseAndReverse(right, m - 1, n - 1);
-
-        // In case both the pointers cross each other or become equal, we
-        // stop i.e. don't swap data any further. We are done reversing at this
-        // point.
-        if (this.left == right || right.next == this.left) {
-            this.stop = true;            
-        }
-
-        // Until the boolean stop is false, swap data between the two pointers
-        if (!this.stop) {
-            int t = this.left.val;
-            this.left.val = right.val;
-            right.val = t;
-
-            // Move left one step to the right.
-            // The right pointer moves one step back via backtracking.
-            this.left = this.left.next;
-        }
-    }
-
-    public ListNode reverseBetween(ListNode head, int m, int n) {
-        this.left = head;
-        this.stop = false;
-        this.recurseAndReverse(head, m, n);
-        return head;
+        return dummy.next; 
     }
 }
