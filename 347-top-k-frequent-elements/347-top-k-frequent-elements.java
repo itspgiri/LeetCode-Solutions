d612cf7ef3f7e5a6caf9fb490ee54dd1527a33d0
Priyanka Giri <80326865+itspgiri@users.noreply.github.com>
2022-07-21 16:38:26 +0400

    Time: 31 ms (12.78%), Space: 50.1 MB (70.06%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,39 +1,22 @@
 class Solution {
+
     public int[] topKFrequent(int[] nums, int k) {
-         Map<Integer, Integer> map = new HashMap<>();
-        for(int n: nums){
-            map.put(n, map.getOrDefault(n,0)+1);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        //We created a map of the frequency of every single element
-        
-        //We are basically making a TreeMap of all the numbers with same frequency
-        TreeMap<Integer, List<Integer>> freqMap = new TreeMap<>();
-        //We create a TreeMap, where the Key is the frequency
-        for(int num : map.keySet()){
-           int freq = map.get(num);
-           if(!freqMap.containsKey(freq)){
-               freqMap.put(freq, new LinkedList<>());
-           }
-           freqMap.get(freq).add(num);
+
+        Queue<Integer> heap = new PriorityQueue<>((a, b) -> map.get(b) - map.get(a));
+        for (int key : map.keySet()) {
+            heap.add(key);
         }
-        
-        //Now we create a result ArrayList to get the top K frequent elements
-        List<Integer> res = new ArrayList<>();
-        
-        //We will iterate from the back and keep ading the numbers to res till we get the K elements
-        while(res.size()<k){
-            
-            //We are creating an entry to our FrequencyMap - TreeMap for the last Entry that it contains
-            //The last entry will have the max frequency right? Obviously.
-            //So we get the entry to that element
-            Map.Entry<Integer, List<Integer>> entry = freqMap.pollLastEntry();
-            
-            //Now we are adding all the numbers that are at that particular frequency.
-            res.addAll(entry.getValue());
+
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < k; i++) {
+            ans.add(heap.poll());
         }
-        
-        //We have to convert it to array since the function requires so.
-        int[] res1 = res.stream().mapToInt(Integer::intValue).toArray();
+        int[] res1 = ans.stream().mapToInt(Integer::intValue).toArray();
+
         return res1;
     }
 }
