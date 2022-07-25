44c71be2cd53e5a9c4cb390865ffefd0e3e510d5
Priyanka Giri <80326865+itspgiri@users.noreply.github.com>
2022-07-25 13:15:44 +0400

    Time: 1 ms (38.49%), Space: 47.2 MB (49.67%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,28 +1,65 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int result[]=new int[2];
-        result[0]=-1;
-        result[1]=-1;
-        
-        if(nums.length==1 && nums[0]==target){
-            result[0]=0;
-            result[1]=0;
-            return result;
+   public int[] searchRange(int[] nums, int target) {
+        int start=0;
+        int[] arr=new int[]{-1,-1};
+        int end=nums.length-1;
+        int mid=(start+end)/2;
+        if(nums.length==0){
+            return arr;
         }
-        Set<Integer> st=new HashSet<Integer>();
-        
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target){
-                if(!st.contains(nums[i])){
-                     result[0]=i;
-                     result[1]=i;
-                     st.add(nums[i]);
-                }else{
-                    result[1]=i;
+        if(nums[start]==target || nums[end]==target){
+            if(nums[start]==target){
+                arr[0]=start;
+                while(start<end && nums[start]==target){
+                    if(nums[start+1]==target){
+                        start++;
+                    }
+                    else
+                        break;
                 }
+                arr[1]=start;
             }
+            else{
+                arr[1]=end;
+                while(start<end && nums[end]==target){
+                    if(nums[end-1]==target)
+                        end--;
+                    else
+                        break;
+                }
+                arr[0]=end;
+            }
+            return arr;
         }
-       
-        return result;
+        while(start<mid && mid+1<end && nums[mid]!=target){
+            if(nums[mid]>target){
+                end=mid;
+                
+            }
+            else
+                start=mid;
+            mid=(start+end)/2;
+        }
+        if(nums[mid]==target){
+            while(mid>=1 && nums[mid]==target){
+                if(nums[mid-1]==target){
+                    mid--;
+                }
+                else
+                    break;
+            }
+            arr[0]=mid;
+            mid++;
+            while(mid<=end){
+                if(nums[mid]==target)
+                    mid++;
+                else
+                    break;
+            }
+            arr[1]=mid-1;
+        }
+        
+        return arr;
     }
+
 }
