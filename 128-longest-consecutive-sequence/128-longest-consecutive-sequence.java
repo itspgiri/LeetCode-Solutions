5fe119fc15234ee155b64b69ffd646d923018b05
Priyanka Giri <80326865+itspgiri@users.noreply.github.com>
2022-08-08 20:29:27 +0400

    Time: 19 ms (92.33%), Space: 64.4 MB (76.07%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -1,26 +1,31 @@
 class Solution {
+
     public int longestConsecutive(int[] nums) {
-        if (nums.length == 0) {
+        if (nums.length<=0)
             return 0;
-        }
-
-        Arrays.sort(nums);
-
-        int longestStreak = 1;
-        int currentStreak = 1;
-
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != nums[i-1]) {
-                if (nums[i] == nums[i-1]+1) {
-                    currentStreak += 1;
+        
+       Arrays.sort(nums);
+        
+        int max = 1;
+        int cur = 1;
+        
+        for (int i=1; i<nums.length;i++)
+        {
+            if (nums[i] != nums[i-1])
+            {
+                if (nums[i] == nums[i-1]+1)
+                {
+                    cur++;
                 }
-                else {
-                    longestStreak = Math.max(longestStreak, currentStreak);
-                    currentStreak = 1;
+                else
+                {
+                    max = Math.max(max,cur);
+                    cur = 1;
                 }
             }
         }
-
-        return Math.max(longestStreak, currentStreak);
+        //What if the longest streak never ends right - so it never goes to the else loop right.
+        //That's why we do this.
+        return Math.max(max,cur);
     }
 }
