0301dbb80eefa3ab3a9ed7e0a6571a4ee0e8dfa8
Priyanka Giri <80326865+itspgiri@users.noreply.github.com>
2022-08-25 17:51:14 +0530

    Time: 45 ms (62.46%), Space: 50.9 MB (89.85%) - LeetHub

--- 639-decode-ways-ii/639-decode-ways-ii.java
+++ 639-decode-ways-ii/639-decode-ways-ii.java
@@ -1,33 +1,28 @@
 public class Solution {
     int M = 1000000007;
     public int numDecodings(String s) {
-        Long[] memo = new Long[s.length()];
-        return (int) ways(s, s.length() - 1, memo);
-    }
-    public long ways(String s, int i, Long[] memo) {
-        if (i < 0)
-            return 1;
-        if (memo[i] != null)
-            return memo[i];
-        if (s.charAt(i) == '*') {
-            long res = 9 * ways(s, i - 1, memo) % M;
-            if (i > 0 && s.charAt(i - 1) == '1')
-                res = (res + 9 * ways(s, i - 2, memo)) % M;
-            else if (i > 0 && s.charAt(i - 1) == '2')
-                res = (res + 6 * ways(s, i - 2, memo)) % M;
-            else if (i > 0 && s.charAt(i - 1) == '*')
-                res = (res + 15 * ways(s, i - 2, memo)) % M;
-            memo[i] = res;
-            return memo[i];
+        long first = 1, second = s.charAt(0) == '*' ? 9 : s.charAt(0) == '0' ? 0 : 1;
+        for (int i = 1; i < s.length(); i++) {
+            long temp = second;
+            if (s.charAt(i) == '*') {
+                second = 9 * second % M;
+                if (s.charAt(i - 1) == '1')
+                    second = (second + 9 * first) % M;
+                else if (s.charAt(i - 1) == '2')
+                    second = (second + 6 * first) % M;
+                else if (s.charAt(i - 1) == '*')
+                    second = (second + 15 * first) % M;
+            } else {
+                second = s.charAt(i) != '0' ? second : 0;
+                if (s.charAt(i - 1) == '1')
+                    second = (second + first) % M;
+                else if (s.charAt(i - 1) == '2' && s.charAt(i) <= '6')
+                    second = (second + first) % M;
+                else if (s.charAt(i - 1) == '*')
+                    second = (second + (s.charAt(i) <= '6' ? 2 : 1) * first) % M;
+            }
+            first = temp;
         }
-        long res = s.charAt(i) != '0' ? ways(s, i - 1, memo) : 0;
-        if (i > 0 && s.charAt(i - 1) == '1')
-            res = (res + ways(s, i - 2, memo)) % M;
-        else if (i > 0 && s.charAt(i - 1) == '2' && s.charAt(i) <= '6')
-            res = (res + ways(s, i - 2, memo)) % M;
-        else if (i > 0 && s.charAt(i - 1) == '*')
-            res = (res + (s.charAt(i) <= '6' ? 2 : 1) * ways(s, i - 2, memo)) % M;
-        memo[i] = res;
-        return memo[i];
+        return (int) second;
     }
 }
