685f92d4138b14fc038478edf2b6fcbc3f06b800
Priyanka Giri <80326865+itspgiri@users.noreply.github.com>
2022-08-25 17:51:35 +0530

    Time: 16 ms (99.38%), Space: 59.2 MB (75.69%) - LeetHub

--- 639-decode-ways-ii/639-decode-ways-ii.java
+++ 639-decode-ways-ii/639-decode-ways-ii.java
@@ -1,28 +1,31 @@
-public class Solution {
-    int M = 1000000007;
+class Solution {
     public int numDecodings(String s) {
-        long first = 1, second = s.charAt(0) == '*' ? 9 : s.charAt(0) == '0' ? 0 : 1;
-        for (int i = 1; i < s.length(); i++) {
-            long temp = second;
-            if (s.charAt(i) == '*') {
-                second = 9 * second % M;
-                if (s.charAt(i - 1) == '1')
-                    second = (second + 9 * first) % M;
-                else if (s.charAt(i - 1) == '2')
-                    second = (second + 6 * first) % M;
-                else if (s.charAt(i - 1) == '*')
-                    second = (second + 15 * first) % M;
-            } else {
-                second = s.charAt(i) != '0' ? second : 0;
-                if (s.charAt(i - 1) == '1')
-                    second = (second + first) % M;
-                else if (s.charAt(i - 1) == '2' && s.charAt(i) <= '6')
-                    second = (second + first) % M;
-                else if (s.charAt(i - 1) == '*')
-                    second = (second + (s.charAt(i) <= '6' ? 2 : 1) * first) % M;
+        if (s.charAt(0) == '0') return 0;
+        long[] dp = new long[s.length() + 1];
+        dp[0] = 1; dp[1] = s.charAt(0) == '*' ? 9 : 1;
+        char[] ch = s.toCharArray();
+        
+        for (int i = 2; i <= ch.length; i++) {
+            if (ch[i - 1] == '0') {
+                if (ch[i - 2] == '1' || ch[i - 2] == '2') dp[i] = dp[i - 2];
+                else if (ch[i - 2] == '*') dp[i] = 2 * dp[i - 2];
+                else return 0;
+            } else if (ch[i - 1] >= '1' && ch[i - 1] <= '6') {
+                dp[i] = dp[i - 1];
+                if (ch[i - 2] == '1' || ch[i - 2] == '2') dp[i] += dp[i - 2];
+                else if (ch[i - 2] == '*') dp[i] += 2 * dp[i - 2];
+            } else if (ch[i - 1] >= '7' && ch[i - 1] <= '9') {
+                dp[i] = dp[i - 1];
+                if (ch[i - 2] == '1' || ch[i - 2] == '*') dp[i] += dp[i - 2];
+            } else if (ch[i - 1] == '*') {
+                dp[i] = 9 * dp[i - 1];
+                if (ch[i - 2] == '1') dp[i] += 9 * dp[i - 2];
+                else if (ch[i - 2] == '2') dp[i] += 6 * dp[i - 2];
+                else if (ch[i - 2] == '*') dp[i] += 15 * dp[i - 2];
             }
-            first = temp;
+            dp[i] %= 1000000007;
         }
-        return (int) second;
+        
+        return (int) dp[s.length()];
     }
 }
