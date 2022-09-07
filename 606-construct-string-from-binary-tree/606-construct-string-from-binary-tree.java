654c5e8e906284810834b69cf0bfbf12d4998c77
Priyanka Giri <80326865+itspgiri@users.noreply.github.com>
2022-09-07 09:17:45 +0530

    Time: 3 ms (89.56%), Space: 44.8 MB (77.04%) - LeetHub

---
+++ 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.java
@@ -0,0 +1,25 @@
+class Solution {
+    StringBuilder sb;
+    private void doEv(TreeNode root){
+        if(root == null)return;
+        if(root.left == null && root.right==null){
+            sb.append(root.val);
+            return;
+        }
+        sb.append(root.val);
+        sb.append('(');
+        doEv(root.left);
+        sb.append(')');
+        if(root.right!=null){
+        sb.append('(');
+        doEv(root.right);
+        sb.append(')');
+        }
+        return;
+    }
+    public String tree2str(TreeNode t) {
+        sb = new StringBuilder();
+        doEv(t);
+        return sb.toString();
+    }
+}
