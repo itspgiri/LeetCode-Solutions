c9c4f85ee44f569da77424c4c3d165e20c24b625
Priyanka Giri <80326865+itspgiri@users.noreply.github.com>
2022-11-14 09:27:37 +0530

    Time: 179 ms (11.66%), Space: 54.7 MB (10.82%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -0,0 +1,36 @@
+class Solution {
+    int count = 0;
+    public int removeStones(int[][] stones) {
+        Map<String, String> parent = new HashMap<>();
+        count = stones.length;
+        // init Union Find
+        for (int[] stone : stones) {
+            String s = stone[0] + " " + stone[1];
+            parent.put(s, s);
+        }
+        for (int[] s1 : stones) {
+            String ss1 = s1[0] + " " + s1[1];
+            for (int[] s2 : stones) {
+                if (s1[0] == s2[0] || s1[1] == s2[1]) { // in the same column or row
+                    String ss2 = s2[0] + " " + s2[1];
+                    union(parent, ss1, ss2);
+                }
+            }
+        }
+        return stones.length - count;
+    }
+    private void union(Map<String, String> parent, String s1, String s2) {
+        String r1 = find(parent, s1), r2 = find(parent, s2);
+        if (r1.equals(r2)) {
+            return;
+        }
+        parent.put(r1, r2);
+        count--;
+    }
+    private String find(Map<String, String> parent, String s) {
+        if (!parent.get(s).equals(s)) {
+            parent.put(s, find(parent, parent.get(s)));
+        }
+        return parent.get(s);
+    }
+}
